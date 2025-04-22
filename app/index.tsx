84b11f0863df57bf48c5fe12a806ef7cade8691c
kauanrosendo <kauanfrosendo@gmail.com>
2025-04-22 11:54:39 -0300

    first commit

--- app/index.tsx
+++ app/index.tsx
@@ -3,85 +3,122 @@ import styled from 'styled-components/native';
 import Title from "../components/titulo/titulo";
 import { useEffect, useState } from 'react';
 import InputTexto from '@/components/titulo/Input/input';
-import { Pressable } from 'react-native';
+import { Alert, Pressable } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
+import { api } from '@/utils/api';
+
 export default function Login() {
 
-     const [email,setEmail] = useState('exemplo@exemplo.com')
-     const [erroEmail, setErroEmail] = useState(false)
+    const [email, setEmail] = useState('exemplo@exemplo.com')
+    const [erroEmail, setErroEmail] = useState(false)
 
-     const [senha, setSenha] = useState('@Exemplo123')
-     const [erroSenha, setErroSenha] = useState(false)
-     const [mostrarSenha, setMostrarSenha] = useState(false)
+    const [senha, setSenha] = useState('@Exemplo123')
+    const [erroSenha, setErroSenha] = useState(false)
+    const [mostrarSenha, setMostrarSenha] = useState(false)
 
-     useEffect(()=>{
+    const [formularioValido, setFormularioValido] = useState(true)
+
+    useEffect(() => {
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-        if(emailRegex.test(email))
-        {
-           setErroEmail(false)
+        if (emailRegex.test(email)) {
+            setErroEmail(false)
+            setFormularioValido(false)
         }
-        else{
+        else {
             setErroEmail(true)
+            setFormularioValido(true)
         }
 
-     },[email])
-     
+    }, [email])
 
-     useEffect(()=>{
-       
+
+    useEffect(() => {
+
         const passwordRegex = /^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/;
-        if(!passwordRegex.test(senha))
-        {
-            setErroSenha(true)
+        if (senha == '!@Exemplo123') {
+            setFormularioValido(true)
         }
-        else
-        {
+        else if (passwordRegex.test(senha)) {
             setErroSenha(false)
+            setFormularioValido(false)
         }
-    },[senha])
+        else {
+            setErroSenha(true)
+            setFormularioValido(true)
+        }
+    }, [senha])
 
+    async function Logar() {
+        try {
+            const resposta = await api.post('/Usuario', {
+            email: email,
+            senha: senha
+            })
+
+            console.log(resposta.data)
+        }
+        catch (error) {
+           Alert.alert(
+            "ops",
+            "email ou senha incorreto",
+           )
+        }
+    }
 
     return (
         <Tela>
-            <Title texto={'Entrar'} flag={true}/>
-            <Title texto={'Bem vindo ao app'} flag={false}/>
-                <ContainerCampoTexto>
-                 <Email>   
-                <InputTexto erro= {true} placeholder='Digite seu email...'onChangeText={text =>setEmail(text)} />
-                {erroEmail ?
-                <TextErrorHint>Email invalido</TextErrorHint>
-                 :
-                 null
-                }
+            <Title texto={'Criar conta'} flag={true} />
+            <Title texto={'Bem vindo ao app'} flag={false} />
+            <ContainerCampoTexto>
+                <Email>
+                    <InputTexto erro={true} placeholder='Digite seu email...' onChangeText={text => setEmail(text)} />
+                    {erroEmail ?
+                        <TextErrorHint>Email invalido</TextErrorHint>
+                        :
+                        null
+                    }
                 </Email>
                 <Senha>
-                <InputTexto erro= {false} placeholder='digite sua senha...'onChangeText={text =>setSenha(text)} secureTextEntry={!mostrarSenha}/>
-                <Olho onPress={() => setMostrarSenha(!mostrarSenha)}>
-                <Ionicons name={ mostrarSenha ? "eye-off" : "eye"}  size={24} color="black" />
-                </Olho>
-                {erroSenha ?
-                <TextErrorHint>Senha invalida</TextErrorHint>
-                 :
-                 null
-                }
+                    <InputTexto erro={false} placeholder='Digite sua senha...' onChangeText={text => setSenha(text)} secureTextEntry={!mostrarSenha} />
+                    <Olho onPress={() => setMostrarSenha(!mostrarSenha)}>
+                        <Ionicons name={mostrarSenha ? "eye-off" : "eye"} size={24} color="black" />
+                    </Olho>
+                    {erroSenha ?
+                        <TextErrorHint>Senha invalida</TextErrorHint>
+                        :
+                        null
+                    }
+                </Senha>
+                <Senha>
+                    <InputTexto erro={false} placeholder='Confirme sua senha...' onChangeText={text => setSenha(text)} secureTextEntry={!mostrarSenha} />
+                    <Olho onPress={() => setMostrarSenha(!mostrarSenha)}>
+                        <Ionicons name={mostrarSenha ? "eye-off" : "eye"} size={24} color="black" />
+                    </Olho>
+                    {erroSenha ?
+                        <TextErrorHint>Senha invalida</TextErrorHint>
+                        :
+                        null
+                    }
                 </Senha>
 
-                </ContainerCampoTexto>
-              
-                <Acoes>
-                    <Button>
-                        <ButtonText>entrar</ButtonText>
-                    </Button>
-                    <Cadastrar>Cadastra-se</Cadastrar>
-                    <EsqueciSenha>Esqueci-senha</EsqueciSenha>
-                </Acoes>
+            </ContainerCampoTexto>
+
+            <Acoes>
+                <Button
+                    disabled={formularioValido}
+                    onPress={() => Logar()}>
+                    <ButtonText>Criar minha conta</ButtonText>
+            
+                </Button>
+                
+            </Acoes>
         </Tela>
     )
 }
 const Tela = styled.View`
     flex: 1;
-    background-color: #0d58ee;
+    background-color: #ebde55;
     padding: 26px;
 `;
 
@@ -99,8 +136,9 @@ const Button = styled.Pressable`
     height: 67px;
     align-items: center;
     align-self: center;
+    border-radius: 10px;
     justify-content: center;
-    background-color: #fff;
+    background-color: #d44949;
 `;
 
 const ButtonText = styled.Text`
@@ -129,6 +167,7 @@ top: 18px;
 const Senha = styled.View`
     position: relative;
     width: 100%;
+    
 `;
 
 const Email = styled.View`
